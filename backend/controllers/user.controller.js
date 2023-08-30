58670090ff0096092d20ccdf80c4d53200d38ffa
gulshan07dev <gulshanku9135@gmail.com>
2023-08-30 12:41:37 +0530

    update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -10,7 +10,7 @@ import sendEmail from "../utils/sendEmail.js";
 const cookieOptions = {
     httpOnly: true,
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-    secure: true, 
+    // secure: true, 
 }
 
 
