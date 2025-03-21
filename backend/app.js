00a3af848027816cb58b8e839ff7b929bf0e31bd
Gulshan kumar <gulshanku9135@gmail.com>
2025-03-21 21:33:56 +0530

    delete unused video file, update CORS origin to use FRONTEND_URL, and upgrade nodemailer and add googleapis dependency

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan('dev'));
-app.use(cors({ origin: [process.env.CLIENT_URL], credentials: true }));
+app.use(cors({ origin: [process.env.FRONTEND_URL], credentials: true }));
 
 
 app.use('/api/v1/user', userRoutes);
