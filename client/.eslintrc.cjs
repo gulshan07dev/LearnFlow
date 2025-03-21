7fe8e1e3a743716f05a6e19ff811fca37183ce19
Gulshan kumar <gulshanku9135@gmail.com>
2025-03-21 21:36:19 +0530

    update ESLint configuration and modify package-lock.json

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,8 +1,7 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
+  extends: [ 
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
